cc8f2f9ff8190c695814334d5d507b95436c177b
Yvette <nchombuayvta@gmil.com>
2020-10-13 13:29:30 +0200

    header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import{Container} from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 function App() {
   return (
     <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+      <h1>Welcome To Proshop</h1>
+      </Container>
+      </main>
+      <Footer/>
      
     </>
   );
