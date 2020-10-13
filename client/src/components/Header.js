f7f786f58a28cf689d84f75d9e92d8d10ddafbaf
Yvette <nchombuayvta@gmil.com>
2020-10-13 15:33:50 +0200

    products added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant ="dark" expand="lg" collapseOnSelect>
-        <Container>
-      <Navbar.Brand href="/">PROSHOP</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link href="/cart"><i className="fas fa-shopping-cart mr-2"></i>CART</Nav.Link>
-          <Nav.Link href="/login"><i className="fas fa-user mr-2"></i>SIGN IN</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>PROSHOP</Navbar.Brand>
+        </LinkContainer>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Link to="/cart">
+              <i className="fas fa-shopping-cart mr-2 nav-link"></i>CART
+            </Link>
+            <Link to="/login">
+              <i className="fas fa-user mr-2"></i>SIGN IN
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
