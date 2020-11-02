0939b7c3e2eff92d244d3b3b024eba9d05e4b9df
Yvette <nchombuayvta@gmil.com>
2020-11-02 10:44:58 +0100

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -38,8 +38,8 @@ function App() {
           <Route path='/order/:id' component={OrderScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
-          <Route path="/productlist" component={ProductListScreen} />
-          <Route path="/admin/productlist" component={ProductListScreen} />
+          {/* <Route path="/productlist" component={ProductListScreen} />
+          <Route path="/admin/productlist" component={ProductListScreen} /> */}
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/orderlist" component={OrderListScreen} />
           <Route path="/search/:keyword" component={HomeScreen}  exact/>
